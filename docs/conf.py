f7ba6512b22e60d26b7167b1e52a5a7b0e65a903
sigita42 <sigita42@users.noreply.github.com>
2016-09-26 22:48:32 +0200

    Add some lines from the RTD template conf file

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,34 @@
+import os
+import sys
+
 from recommonmark.parser import CommonMarkParser
+
+sys.path.insert(0, os.path.abspath('..'))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "readthedocs.settings.dev")
+
+from django.conf import settings
+
+import django
+django.setup()
+
+
+sys.path.append(os.path.abspath('_ext'))
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinxcontrib.httpdomain',
+    'djangodocs',
+]
+templates_path = ['_templates']
 
 source_parsers = {
     '.md': CommonMarkParser,
 }
 
 source_suffix = ['.rst', '.md']
+
+master_doc = 'index'
+project = u'WebBSC documentation'
+
+language = 'en'
+
