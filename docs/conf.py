33931eb6c28ea54dc8b8c5b1b87adb2a33fe5723
sigita42 <sigita42@users.noreply.github.com>
2016-10-04 21:34:36 +0200

    delete potentially unnecessary lines

--- docs/conf.py
+++ docs/conf.py
@@ -1,14 +1,6 @@
 import os
 import sys
 
-from recommonmark.parser import CommonMarkParser
-
-source_parsers = {
-    '.md': CommonMarkParser,
-}
-
-source_suffix = ['.rst', '.md']
-
 master_doc = 'index'
 project = u'WebBSC documentation'
 
