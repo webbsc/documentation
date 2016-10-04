141d92dad8c6cd8280d4df3c2be38eefe53d0904
sigita42 <sigita42@users.noreply.github.com>
2016-10-04 21:54:19 +0200

    adding configuration from template

--- docs/conf.py
+++ docs/conf.py
@@ -4,5 +4,10 @@ import sys
 master_doc = 'index'
 project = u'WebBSC documentation'
 
+html_static_path = ['_static']
+
+html_theme = 'default'
+htmlhelp_basename = 'ReadtheDocsTemplatedoc'
+
 language = 'en'
 
