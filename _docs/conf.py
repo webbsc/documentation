0887192ad420dd4f4ee59cf4ef13985fedd796ea
sigita42 <sigita42@users.noreply.github.com>
2016-10-05 15:15:48 +0200

    create conf.py test

---
+++ _docs/conf.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+from recommonmark.parser import CommonMarkParser
+
+templates_path = ['_templates']
+
+source_suffix = ['.rst', '.md', '.markdown']
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+master_doc = 'index'
+project = u'WebBSC documentation'
+
+html_static_path = ['_static']
+
+html_theme = 'default'
+htmlhelp_basename = 'ReadtheDocsTemplatedoc'
+
+language = 'en'
